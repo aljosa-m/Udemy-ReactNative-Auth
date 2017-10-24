c9879edd9cf646ae3be0b0653a866a709282b712
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-10-24 16:54:36 +0800

    added option to login and logout and spinner

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common';
+import { Header, Button, Spinner } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 export default class App extends React.Component {
-	componendWillMount() {
+  state = { loggedIn: null}
+
+	componentWillMount() {
 		firebase.initializeApp({
 			apiKey: 'AIzaSyBtB0KN1x1bH8eC0XhbaUW2wDz-mNRQbZs',
 			authDomain: 'auth-2df5f.firebaseapp.com',
@@ -14,12 +16,36 @@ export default class App extends React.Component {
 			storageBucket: 'auth-2df5f.appspot.com',
 			messagingSenderId: '114691438374'
 		});
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
 	}
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+      return (
+        <Button
+          onPress={() => firebase.auth().signOut()}>
+          Log Out
+        </Button>
+      );
+      case false:
+        return <LoginForm />
+      default:
+        return <Spinner size="large" />
+    }
+  }
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
 			</View>
 		);
 	}
